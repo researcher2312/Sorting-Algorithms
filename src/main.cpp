032045d191106b61b215c75d6bd9c4b4cf8c6d2f
msegedyn <researcher2312@gmail.com>
2019-03-30 11:22:52 +0100

    measuring sorting time is now based on average from 100 calculations, changed output to stderr

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,19 @@
 using namespace std;
 
 int main(){
-
-for(auto func: sorting){
-  for(auto fill: filling){
-    for(auto size: sizes){
-      TYPE* tab = generateTab<TYPE>(size, fill.percent, fill.dir);
-      cout << sortDuration(func, tab, size) << ' ';
-      delete tab;
+  for(auto func: sorting){
+    for(auto fill: filling){
+      for(auto size: sizes){
+        float time = 0;
+        for(int i=0; i<100; i++){
+          TYPE* tab = generateTab<TYPE>(size, fill.percent, fill.dir);
+          time += sortDuration(func, tab, size);
+          delete tab;
+        }
+        time /= 100;
+        cerr << time << ' ';
+      }
     }
   }
-}
   return 0;
 }
