80a7a5f39b081cddae827141c245ca99854633c8
msegedyn <researcher2312@gmail.com>
2019-03-30 10:46:27 +0100

    added procedure for testing each configuration

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,22 @@
-#include <iostream>
 #include "sort.h"
 #include "utils.h"
+#include "config.h"
 #include <ctime>
+#include <iostream>
 
 
 using namespace std;
 
 int main(){
-  int size = 10000000;
-  int* tab = generateTab<int>(size);
 
-  // int tab[10]={2,5,7,4,7,9,12,3,4,5};
-
-
-  cout << sortDuration(mergesort, tab, 0, size-1);
-
+for(auto func: sorting){
+  for(auto fill: filling){
+    for(auto size: sizes){
+      TYPE* tab = generateTab<TYPE>(size, fill.percent, fill.dir);
+      cout << sortDuration(func, tab, size) << ' ';
+      delete tab;
+    }
+  }
+}
   return 0;
 }
